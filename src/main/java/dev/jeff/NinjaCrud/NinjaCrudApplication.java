a9fc07f2e5df929d1181a6f6c43a4e16c093d96b
Jefferson Bueno <jeffersonbuenoprofissional@gmail.com>
2025-11-16 10:03:25 -0300

    Initial commit

---
+++ src/main/java/dev/jeff/NinjaCrud/NinjaCrudApplication.java
@@ -0,0 +1,13 @@
+package dev.jeff.NinjaCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jeff/NinjaCrud/NinjaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeff.NinjaCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
