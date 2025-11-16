0b0249ecd93b58c0cd216b271f63bd3016fa4071
Jefferson Bueno <jeffersonbuenoprofissional@gmail.com>
2025-11-16 10:39:13 -0300

    feat: create entity

---
+++ src/main/java/dev/jeff/NinjaCrud/NinjaModel.java
@@ -0,0 +1,48 @@
+package dev.jeff.NinjaCrud;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_ninjas")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String email, int idade, String nome) {
+        this.email = email;
+        this.idade = idade;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
